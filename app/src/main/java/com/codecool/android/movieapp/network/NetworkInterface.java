ce3ded8a94c653f77b4e3424d4665ed0d7849132
antalEhrbar <ehranti@gmail.com>
2018-09-10 15:59:34 +0200

    network interface

--- app/src/main/java/com/codecool/android/movieapp/network/NetworkInterface.java
+++ app/src/main/java/com/codecool/android/movieapp/network/NetworkInterface.java
@@ -1,4 +1,14 @@
 package com.codecool.android.movieapp.network;
 
+import android.database.Observable;
+
+import com.codecool.android.movieapp.models.MovieResponse;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface NetworkInterface {
+
+    @GET("discover/movie")
+    Observable<MovieResponse> getMovies(@Query("api_key") String api_key);
 }
